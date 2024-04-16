e78d5aefe7b8021986a34a5db6cca52e6ca5e006
linHeYang <2121938199@qq.com>
2024-04-16 16:46:24 +0900

    Revert "4月16日"
    This reverts commit a70b59fd85c710ce6fb1b5c2020e0b7d70bc0151.

--- AprilSixteen2.java
+++
@@ -1,8 +0,0 @@
-//public class AprilSixteen2表示AprilSixteen2是一个类，public的类
-public class AprilSixteen2{
-	
-	//主方法main
-	public static void main(String[] args) {
-		System.out.println("Lin is studying java!"); 
-	}
-}
--- AprilSixteen4.java
+++
@@ -1,9 +0,0 @@
-public class AprilSixteen4{
-	public static void main(String[] args) {
-		//System.out.println("书名\t作者\t价格\t销量");
-		//System.out.println("三国\t罗贯中\t120\t1000");
-		System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
-		//视频后习题
-		//以上两个方法都可以   
-	}
-}
