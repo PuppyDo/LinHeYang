951e55d29bfeea258b4ab32434a387d07260ff18
linHeYang <2121938199@qq.com>
2024-04-17 17:17:49 +0900

    4月16日

---
+++ AprilSixteen2.java
@@ -0,0 +1,8 @@
+//public class AprilSixteen2表示AprilSixteen2是一个类，public的类
+public class AprilSixteen2{
+	
+	//主方法main
+	public static void main(String[] args) {
+		System.out.println("Lin is studying java!"); 
+	}
+}
---
+++ AprilSixteen3.java
@@ -0,0 +1,18 @@
+public class AprilSixteen3{
+	public static void main(String[] args) {
+		
+		System.out.println("东京\t大阪\t名古屋");
+		System.out.println("东京\n大阪\n三重");
+		System.out.println("东京\\大阪\\三重");
+		System.out.println("老师说：东京\"大阪\"三重");
+		System.out.println("老师说：\'东京大阪三重\'");
+		System.out.println("名侦探柯南\r东京");
+		
+	}
+}
+//  \t   一个制表位，实现对齐功能
+//  \n   换行
+//  \\   文字内1个斜杠  斜杠增加以此类推
+//  \"   文字内标重点 ''
+//  \'   文字内标重点 '
+//  \r   回车=换行
